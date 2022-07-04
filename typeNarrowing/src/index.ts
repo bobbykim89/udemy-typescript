4becdb9cacbb559fb8f8f021fb699ffb9c3c7ec9
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-07-04 16:38:07 -0700

    finish type narrowing section

--- typeNarrowing/src/index.ts
+++ typeNarrowing/src/index.ts
@@ -59,3 +59,100 @@ const printFullData = (date: string | Date) => {
     console.log(new Date(date).toUTCString())
   }
 }
+
+class User {
+  constructor(public username: string) {}
+}
+class Company {
+  constructor(public name: string) {}
+}
+
+const printName = (entity: User | Company) => {
+  if (entity instanceof User) {
+    entity
+  } else {
+    entity
+  }
+}
+
+/**Type Predicates
+ @TypePredicates typescript allows us to write custom functions that can narrow the type of a value. These functions have a very special return type called a type predicate
+ */
+
+interface Cat {
+  name: string
+  numLives: number
+}
+interface Dog {
+  name: string
+  breed: string
+}
+
+const isCat = (animal: Cat | Dog): animal is Cat => {
+  return (animal as Cat).numLives !== undefined
+}
+
+const makeNoise = (animal: Cat | Dog): string => {
+  if (isCat(animal)) {
+    animal
+    return 'Miau'
+  } else {
+    animal
+    return 'Rau'
+  }
+}
+
+// Discriminated unions
+interface Rooster {
+  name: string
+  weight: number
+  age: number
+  kind: 'rooster'
+}
+interface Cow {
+  name: string
+  weight: number
+  age: number
+  kind: 'cow'
+}
+interface Pig {
+  name: string
+  weight: number
+  age: number
+  kind: 'pig'
+}
+
+interface Sheep {
+  name: string
+  weight: number
+  age: number
+  kind: 'sheep'
+}
+
+type FarmAnimal = Pig | Rooster | Cow | Sheep
+
+const getFarmAnimalSound = (animal: FarmAnimal) => {
+  switch (animal.kind) {
+    case 'pig':
+      return 'Oink!'
+    case 'cow':
+      return 'Moo!'
+    case 'rooster':
+      return 'Cockadoodledoo!'
+    case 'sheep':
+      return 'Baa!'
+    default:
+      // We should never make it here, if we handled all cases correctly.
+      const _exhaustiveCheck: never = animal
+      return _exhaustiveCheck
+  }
+}
+
+const stevie: Rooster = {
+  name: 'Stevie Chicks',
+  weight: 2,
+  age: 1.5,
+  kind: 'rooster',
+}
+
+console.log(getFarmAnimalSound(stevie))
