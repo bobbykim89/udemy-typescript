cdffb6ac3ddfdae49bcfb9c28d05ec41e5747b3c
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-07-06 21:23:44 -0700

    finalize react-typescript

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { v4 as getId } from 'uuid'
 import ShoppingList from './components/ShoppingList'
+import ShoppingListForm from './components/ShoppingListForm'
 import Item from './models/item'
 
 function App() {
   const [items, setItems] = useState<Item[]>([])
+  const addItem = (product: string, quantity: number) => {
+    console.log('Made to the app component')
+    console.log(product)
+    setItems([...items, { id: getId(), product, quantity }])
+  }
   // const items = [
   //   { id: 1, product: 'Lemon', quantity: 3 },
   //   { id: 2, product: 'Chicken Breast', quantity: 2 },
@@ -13,6 +20,7 @@ function App() {
     <div className='App'>
       <h1>Hello World!</h1>
       <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   )
 }
